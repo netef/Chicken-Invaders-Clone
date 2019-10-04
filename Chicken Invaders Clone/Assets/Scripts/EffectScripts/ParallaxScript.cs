c753c7ce8be5a38f6ac703d6814f9d537359b67d
netef <netef10@gmail.com>
2019-10-05 01:47:27 +0300

    added parallax effect

---
+++ Chicken Invaders Clone/Assets/Scripts/EffectScripts/ParallaxScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ParallaxScript : MonoBehaviour
+{
+    private float speed;
+    void Awake()
+    {
+        speed = 0.5f;
+    }
+
+    void Update()
+    {
+        GetComponent<RawImage>().materialForRendering.mainTextureOffset = new Vector2(0, Time.time * speed);
+    }
+}
