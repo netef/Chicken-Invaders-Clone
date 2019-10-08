4002fee3ed71f1aed150f62938c0ec0c0c39b4d5
netef <netef10@gmail.com>
2019-10-08 03:03:30 +0300

    added sounds to the shots

--- Chicken Invaders Clone/Assets/Scripts/PlayerScripts/ShootingScript.cs
+++ Chicken Invaders Clone/Assets/Scripts/PlayerScripts/ShootingScript.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class ShootingScript : MonoBehaviour
 {
+    private AudioSource audio;
+    public AudioClip shotSound;
     public GameObject shot;
     private string playerName;
 
     private void Awake()
     {
+        audio = GetComponent<AudioSource>();
         playerName = gameObject.name;
     }
 
     void Update()
     {
 
-        if (playerName.Equals("Player1"))
+        if (playerName.Equals("Player1") && Input.GetButtonDown("Controller1Square"))
         {
-            if (Input.GetButtonDown("Controller1Square"))
-                Instantiate(shot, gameObject.transform.position, shot.transform.rotation);
+            Instantiate(shot, gameObject.transform.position, shot.transform.rotation);
+            audio.PlayOneShot(shotSound);
         }
 
-        else if (playerName.Equals("Player2"))
-            if (Input.GetButtonDown("Controller2Square"))
-                Instantiate(shot, gameObject.transform.position, shot.transform.rotation);
+        else if (playerName.Equals("Player2") && Input.GetButtonDown("Controller2Square"))
+        {
+            Instantiate(shot, gameObject.transform.position, shot.transform.rotation);
+            audio.PlayOneShot(shotSound);
+        }
     }
 }
